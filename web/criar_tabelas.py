d14465632abf9767ccd22e159cbfac662cfc70be
BrunoLenon <brunolenon30@gmail.com>
2026-03-10 18:32:18 -0400

    apaga

--- web/criar_tabelas.py
+++
@@ -1,4 +0,0 @@
-from db import criar_tabelas
-
-criar_tabelas()
-print("✅ Tabelas criadas com sucesso")
--- web/teste_db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine, text
-
-DB_URL = "postgresql+psycopg2://postgres:%40Veipecas101@localhost:5437/sistemavendas"
-
-engine = create_engine(DB_URL)
-
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT 1"))
-    print("Conexão OK:", result.scalar())
